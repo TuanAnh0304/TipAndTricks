020f5e6ebbf86565d32ea9273b84f4829a238c1a
Vũ Ngọc Tuấn Anh <vungoctuananh030402@gmail.com>
2023-03-06 21:20:14 +0700

    Lab1-Practice

---
+++ src/TipAndTricks/TatBlog.Services/Blogs/IBlogRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TatBlog.Core.Contracts;
+using TatBlog.Core.Entities;
+using TatBlog.Core.DTO;
+
+namespace TatBlog.Services.Blogs
+{
+    public interface IBlogRepository
+    {
+        Task<Post> GetPostAsync(
+            int year,
+            int month,
+            string slug,
+            CancellationToken cancellationToken = default);
+
+        Task<IList<Post>> GetPopularArticlesAsync(
+            int numPosts,
+            CancellationToken cancellationToken = default);
+
+        Task IncreaseViewCountAsync(
+            int postId,
+            CancellationToken cancellationToken = default);
+
+        Task<IList<Category>> GetCategoriesAsync(
+            bool showOnMenu = false, CancellationToken cancellationToken = default);
+        Task<IPagedList<TagItem>> GetPagedTagAsync(
+            IPagingParams pagingParams,
+            CancellationToken cancellationToken = default);
+        Task<Tag> GetTagBySlugAsync(string slug, CancellationToken cancellationToken = default);
+    }
+}
