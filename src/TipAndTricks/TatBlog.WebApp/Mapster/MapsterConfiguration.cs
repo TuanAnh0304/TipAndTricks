a5a7f8bc934d5b6ab5cadf11b1c7bbbde417decf
choa0304 <vungoctuananh030402@gmail.com>
2023-03-14 22:50:31 +0700

    Sử dụng gói thư viện Mapster

--- src/TipAndTricks/TatBlog.WebApp/Mapster/MapsterConfiguration.cs
+++ src/TipAndTricks/TatBlog.WebApp/Mapster/MapsterConfiguration.cs
@@ -1,6 +1,21 @@
-﻿namespace TatBlog.WebApp.Mapster
+﻿using Mapster;
+using TatBlog.Core.Entities;
+using TatBlog.WebApp.Areas.Admin.Models;
+using TatBlog.Services.Blogs;
+
+namespace TatBlog.WebApp.Mapster
 {
-    public class MapsterConfiguration
+    public class MapsterConfiguration : IRegister
     {
+        public void Register(TypeAdapterConfig config)
+        {
+            config.NewConfig<Post, PostItem>()
+                .Map(dest => dest.CategoryName, src => src.Category.Name)
+                .Map(dest => dest.Tags, src => src.Tags.Select(x => x.Name));
+
+            config.NewConfig<PostFilterModel, PostQuery>()
+                .Map(dest => dest.PublishedOnly, src => false);
+        }
+    
     }
 }
