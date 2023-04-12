1403f34482097bb7c3a740e3e7fec68bef4f4ce1
choa0304 <vungoctuananh030402@gmail.com>
2023-04-12 22:44:57 +0700

    Quan ly trang thai

--- src/tat-blog/src/Services/BlogRepository.js
+++ src/tat-blog/src/Services/BlogRepository.js
@@ -16,3 +16,18 @@ get_api(`https://localhost:7085/api/authors?name=${name}&PageSize=${pageSize}&Pa
 export function getFilter() {
     return get_api('https://localhost:7085/api/posts/get-filter');
    }
+   export function getPostsFilter(keyword = '', authorId = '', categoryId = '',
+   year = '', month = '', pageSize = 10, pageNumber = 1, sortColumn = '',
+   sortOrder = '') {
+    let url = new URL('https://localhost:7085/api/posts/get-posts-filter');
+    keyword !== '' && url.searchParams.append('Keyword', keyword);
+    authorId !== '' && url.searchParams.append('AuthorId', authorId);
+    categoryId !== '' && url.searchParams.append('CategoryId', categoryId);
+    year !== '' && url.searchParams.append('Year', year);
+    month !== '' && url.searchParams.append('Month', month);
+    sortColumn !== '' && url.searchParams.append('SortColumn', sortColumn);
+    sortOrder !== '' && url.searchParams.append('SortOrder', sortOrder);
+    url.searchParams.append('PageSize', pageSize);
+    url.searchParams.append('PageNumber', pageNumber);
+    return get_api(url.href);
+}
